f56484bc8a558b6c0a830c7c36b514312ba85889
Isha Kukadia <ikukadia99@gmail.com>
2023-02-26 16:23:03 -0600

    Refactor home page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,29 +1,19 @@
 import './App.css';
-import collaborate from './collaborate.png';
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import SignIn from './components/SignIn/SignIn';
-// import SignUp from './components/SignUp/SignUp';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home/Home';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
-  const title = "Campus\nCollaborator";
   return (
     <div className="App">
-      <header className="App-header">
-        {title}
-      </header>
-      <div className="button-container">
-        <button className="sign-in-button">Sign in</button>
-        <button className="sign-up-button">Create an account</button>
-      </div>
-      <div className="img-container">
-        <img src={collaborate}/>
-      </div>
-      {/* <Router>
-          <Switch>
-            <Route exact path='/signin' component={SignIn} />
-            <Route exact path='/signup' component={SignUp} />
-          </Switch>
-        </Router> */}
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/signin' element={<SignIn />} />
+          <Route exact path='/signup' element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
