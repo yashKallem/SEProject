9af056acdb851828b8c2fab71ed77df4aa9256e4
Isha Kukadia <ikukadia99@gmail.com>
2023-03-13 17:45:59 -0500

    Resolve merge conflicts

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
-import Profile from './components/Profile/Profile';
+import NavigationBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavigationBar />
         <Routes>
-          <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<Home />} />
           <Route exact path='/signin' element={<SignIn />} />
           <Route exact path='/signup' element={<SignUp />} />
           <Route exact path='/profile' element={<Profile />} />
