a94c1724d527b0fd96ce17f54a4fbbe3193e33cf
kallem <ykr-kallem@wiu.edu>
2023-03-21 22:42:42 -0500

    updated the form post

--- react-app/src/components/Project/ProjectScreen.jsx
+++ react-app/src/components/Project/ProjectScreen.jsx
@@ -2,92 +2,199 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Navbar from '../NavBar/NavBar';
 //import './Profile.css';
+import Button from 'react-bootstrap/Button';
+import "../SignUp/SignUp.css"
 
 
 const ProjectScreen = () => {
 
-    console.log(Date.now())
+    const [postProject, setPostProject] = useState(false)
+    const [addProject, setAddProject] = useState(false);
     const [params, setParams] = useState({
-        projectName: "d",
-        projectDescription: "d",
-        projectRole: "d",
-        location: "d",
-        publishedAt: new Date(),
-        jobDescription: "d",
-        deadline: new Date(),
-        email: "d"
+        projectName: "",
+        projectDescription: "",
+        projectRole: "",
+        location: "",
+        jobDescription: "",
+        deadline: "",
+        deadlinedate: "text"
     }
     );
-    const [a, setA] = useState(false)
+
     const location = useLocation();
-    console.log(location.state)
-    // const OnAdd = () => {
+
+    const p = () => {
+        console.log(params)
+    }
+
+    const handleChange = (e) => {
+        setParams({
+            ...params,
+            [e.target.name]: e.target.value
+        });
+    }
+
     useEffect(() => {
-        fetch("http://localhost:8080/api/v1/projects/add", {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${location.state.token}`
-            },
-            body: JSON.stringify({
-                // "projectName": params.projectName,
-                // "projectDescription": "This project is about developing a new mobile application",
-                // "projectRole": "Mobile Developer",
-                // "location": "New York yash",
-                // "publishedAt": "2023-03-17T10:00:00.000Z",
-                // "jobDescription": "We are looking for an experienced mobile developer who can work on both iOS and Android platforms",
-                // "deadline": "2023-04-17T10:00:00.000Z",
-                // "email": "shamsi@wiu.edu"
-
-
-                projectName: "d",
-                projectDescription: "d",
-                projectRole: "d",
-                location: "d",
-                publishedAt: new Date(),
-                jobDescription: "d",
-                deadline: new Date(),
-                email: "shamsi@wiu.edu"
-            }
-            )
-        })
-            .then(response => {
-                return response.json();
-            }).then(data => {
-                console.log(data)
+        console.log(postProject);
+        console.log(params)
+        if (postProject) {
+            fetch("http://localhost:8080/api/v1/projects/add", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${location.state.token}`
+                },
+                body: JSON.stringify(
+                    {
+                        "projectName": params.projectName,
+                        "projectDescription": params.projectDescription,
+                        "projectRole": params.projectRole,
+                        "location": params.location,
+                        "publishedAt": new Date(),
+                        "jobDescription": params.jobDescription,
+                        "deadline": params.deadline,
+                        "email": `${location.state.email}`
+                        // 
+                        // 
+                        // projectName: "d",
+                        // projectDescription: "d",
+                        // projectRole: "d",
+                        // location: "d",
+                        // publishedAt: new Date(),
+                        // jobDescription: "d",
+                        // deadline: new Date(),
+                        // email: "shamsi@wiu.edu"
+                    }
+                )
             })
-            .catch(error => console.log(error));
-    }, [a]);
+                .then(response => {
+
+                    return response.json();
+                }).then(data => {
+                    console.log(data)
+                    setPostProject(false)
+                    setAddProject(false)
+                })
+                .catch(error => console.log(error));
+        }
+    }, [postProject]);
+
+    const changeForm = () => {
+        setAddProject(true)
+
+    }
+    const submitForm = () => {
+        setPostProject(true)
+    }
+
+
+    const handleFocus = () => {
+        setParams({
+            ...params,
+            deadlinedate: 'date'
+        })
+    }
+
+    const handleBlur = () => {
+        if (params.deadline === '') {
+            setParams({
+                ...params,
+                deadlinedate: 'text'
+            })
+        }
+    }
+
     // }
     return (
-
-
-
-
-
-
         <>
-            <p onClick={() => setA(!a)}>djfbg</p>
-            {/* <div style={styles.profile
-            } >
-
-                <div style={{ display: "flex", flex: 1, height: "100%", width: "100%", backgroundColor: "black" }}></div>
-                <div style={{ display: "flex", flex: 1, height: "100%", width: "100%", backgroundColor: "white" }}></div>
-            </div>
- */}
-
-
-
-
-
-
-
-
-
-
             <Navbar />
-            <div className="profile-page">
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    // alignItems: "center",
+                    height: "100vh",
+                    overflow: " hidden",
+                    // margin: "20px"
+                }}
+            >
+                <div
+                    className="flex:1"
+                    style={{ padding: "20px", margin: "40px", height: "400px", border: '1px solid', borderRadius: 20, flex: 2 }}>
 
+                    <Button variant="outline-primary"
+                        onClick={changeForm}
+                        size="lg">
+                        Block level button
+                    </Button>
+
+
+                </div>
+                <div
+                    className="flex:2"
+                    style={{
+                        height: "80%", /* subtract height of other boxes and margin */
+                        overflowY: "scroll",
+                        border: '1px solid', borderRadius: 20, margin: "40px", flex: 3
+                    }}
+                >
+                    <div style={{ padding: "20px" }}>
+
+                        {/* <FormPost /> */}
+
+
+
+                        {addProject ? (
+
+                            <form >
+                                <div className="form-fields"
+                                    style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        justifyContent: "center"
+                                    }}
+                                >
+                                    <input type="text" placeholder="project name" required name="projectName" value={params.projectName} onChange={handleChange} />
+                                    <br />
+                                    <input type="textarea" placeholder="project description" required name="projectDescription" value={params.projectDescription} onChange={handleChange} />
+                                    <br />
+                                    <input type="text" placeholder="location" required name="location" value={params.location} onChange={handleChange} />
+                                    <br />
+                                    <input type="text" placeholder="job description" required name="jobDescription" value={params.jobDescription} onChange={handleChange} />
+                                    <br />
+                                    <input type="text" placeholder="project role" required name="projectRole" value={params.projectRole} onChange={handleChange} />
+                                    <br />
+                                    {/* <input type="date" required name="Deadline" value={params.deadline} onChange={handleChange} /> */}
+                                    <input type="date" onChange={(event) => setParams({ ...params, deadline: event.target.value })} />
+                                    <br />
+                                    <input type="button" value="Add Project" onClick={submitForm} />
+                                </div >
+                            </form>) : (null)
+
+                        }
+                        {/* {addProject == true &&
+                            <form >
+                                <input type="text" placeholder="project name" required name="projectName" value={params.projectName} onChange={handleChange} />
+                                <input type="text" placeholder="project description" required name="projectDescription" value={params.projectDescription} onChange={handleChange} />
+                                <input type="text" placeholder="location" required name="location" value={params.location} onChange={handleChange} />
+                                <input type="text" placeholder="job description" required name="jobDescription" value={params.jobDescription} onChange={handleChange} />
+                                <input type="text" placeholder="project role" required name="projectRole" value={params.projectRole} onChange={handleChange} />
+                                <br />
+                                <input type="button" value="Add Project" onClick={setPostProject(true)} />
+
+                            </form>
+                        } */}
+
+                    </div>
+                </div>
+                <div
+                    className="flex:3"
+                    style={{ height: "400px", border: '1px solid', borderRadius: 20, margin: "40px", flex: 2, padding: "20px" }}>
+
+                    <p>
+
+                    </p>
+                </div>
             </div>
         </>
     );
